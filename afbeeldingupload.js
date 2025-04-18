f1fa12e070280f0856a69e46346cc5c9ef1a7440
FAtaxi <fataxiservice@outlook.com>
2025-04-18 21:40:51 +0200

    afbeeldingupload.js

--- afbeeldingupload.js
+++ afbeeldingupload.js
@@ -1,42 +1,42 @@
 import { v2 as cloudinary } from 'cloudinary';
 
 (async function() {
+    // Configuratie van Cloudinary
+    cloudinary.config({
+        cloud_name: 'db48g9zis', // Jouw Cloudinary cloud_name
+        api_key: '896781555618925', // Jouw Cloudinary API key
+        api_secret: 'Cl3zUh6ToOfJ8AFNCCnDIwqfwVg' // Jouw Cloudinary API secret
+    });
 
-    // Configuration met directe API-sleutels
-    cloudinary.config({ 
-        cloud_name: 'db48g9zis', 
-        api_key: '896781555618925', 
-        api_secret: 'Cl3zUh6ToOfJ8AFNCCnDIwqfwVg' // API secret hier toegevoegd
-    });
-    
-    // Upload een afbeelding
-    const uploadResult = await cloudinary.uploader
-       .upload(
-           'https://res.cloudinary.com/demo/image/upload/getting-started/shoes.jpg', {
-               public_id: 'shoes',
-           }
-       )
-       .catch((error) => {
-           console.log(error);
-       });
-    
-    console.log(uploadResult);
-    
-    // Optimaliseer levering door te verkleinen en automatisch formaat en kwaliteit toe te passen
-    const optimizeUrl = cloudinary.url('shoes', {
-        fetch_format: 'auto',
-        quality: 'auto'
-    });
-    
-    console.log(optimizeUrl);
-    
-    // Transformeer de afbeelding: automatisch bijsnijden naar vierkant
-    const autoCropUrl = cloudinary.url('shoes', {
-        crop: 'auto',
-        gravity: 'auto',
-        width: 500,
-        height: 500,
-    });
-    
-    console.log(autoCropUrl);    
+    try {
+        // Upload een afbeelding
+        const uploadResult = await cloudinary.uploader.upload(
+            'https://res.cloudinary.com/demo/image/upload/getting-started/shoes.jpg', 
+            {
+                public_id: 'shoes' // Kies een unieke naam voor het bestand
+            }
+        );
+
+        console.log(uploadResult);
+
+        // Optimaliseer levering door formaten en kwaliteit automatisch aan te passen
+        const optimizeUrl = cloudinary.url('shoes', {
+            fetch_format: 'auto',
+            quality: 'auto'
+        });
+
+        console.log(optimizeUrl);
+
+        // Transformeer de afbeelding: automatisch bijsnijden naar een vierkant
+        const autoCropUrl = cloudinary.url('shoes', {
+            crop: 'auto',
+            gravity: 'auto',
+            width: 500,
+            height: 500
+        });
+
+        console.log(autoCropUrl);
+    } catch (error) {
+        console.error("Fout bij het uploaden van de afbeelding:", error);
+    }
 })();
